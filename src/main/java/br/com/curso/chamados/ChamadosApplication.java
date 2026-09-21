b1895c4207b4c78877c168973543910a78ebc597
Stanley Alves de Andrade <stanley.andrade@rdamasio.com.br>
2026-09-21 14:03:51 +0000

    Módulo 1 · Bloco 2: projeto Spring Boot gerado (start.spring.io)

---
+++ src/main/java/br/com/curso/chamados/ChamadosApplication.java
@@ -0,0 +1,12 @@
+package br.com.curso.chamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamadosApplication.class, args);
+    }
+}
